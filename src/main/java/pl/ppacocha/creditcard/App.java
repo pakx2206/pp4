4e38253657cdd17bb0461a5e3f8604118f50bec1
Patryk Pacocha <115879093+pakx2206@users.noreply.github.com>
2024-06-09 10:44:56 +0200

    Delete src directory

--- src/main/java/pl/ppacocha/creditcard/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Collections;
-public class App {
-public static void main(String[] args){
-    var name = "Patryk";
-    var message = String.format("Hello %s",name);
-    var names = Collections.singletonList("Patryk Pacocha");
-    System.out.println(message);
-}
-}
